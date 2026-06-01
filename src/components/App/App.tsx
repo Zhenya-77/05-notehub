37e671bd7bf60c2a10ebc5657f4290774bbd3f27
zhenya-kr <zhekakrochak79@gmail.com>
2026-06-01 10:29:25 +0300

    done fix

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,7 +7,7 @@ import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { useDebounce } from "use-debounce";
 import NoteForm from "../NoteForm/NoteForm";
-import SearchBox from "../blabalbal/bababa";
+import SearchBox from "../blabalbal/SearchBox";
 
 function App() {
   const [currentPage, setCurrentPage] = useState(1);
